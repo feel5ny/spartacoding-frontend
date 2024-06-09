9d73d6e92ea1441c674656827e67a8c39ae88f7a
Joy Kim <30822283+feel5ny@users.noreply.github.com>
2024-06-09 21:17:28 +0900

    storybook setting

--- week_1/src/components/submit-button.tsx
+++ week_1/src/components/submit-button.tsx
@@ -1,5 +1,6 @@
 import { Button, ButtonProps, styled } from '@mui/material';
 import { grey } from '@mui/material/colors';
+
 export const SubmitButton = styled(Button)<ButtonProps>(({ theme }) => ({
   color: theme.palette.getContrastText(grey[900]),
   backgroundColor: grey[900],
