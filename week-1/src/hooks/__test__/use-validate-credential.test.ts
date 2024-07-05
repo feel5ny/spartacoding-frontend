6733a814dda2f68a2dcc7f91d866b3f44224a869
Joy Kim <30822283+feel5ny@users.noreply.github.com>
2024-07-05 23:48:17 +0900

    [단위-hook 테스트] 4. 실패케이스 작성

--- week-1/src/hooks/__test__/use-validate-credential.test.ts
+++ week-1/src/hooks/__test__/use-validate-credential.test.ts
@@ -1,5 +1,10 @@
 import { act, renderHook } from '@testing-library/react';
-import { useValidateCredential } from '../use-validate-credential';
+import {
+  EMAIL_ERROR_TEXT,
+  PASSWORD_ERROR_TEXT,
+  useValidateCredential,
+} from '../use-validate-credential';
+
 // useValidateCredential -> 에러텍스트를 관리하는 hook
 describe('useValidateCredential', () => {
   // 성공
@@ -24,7 +29,70 @@ describe('useValidateCredential', () => {
     expect(result.current.emailErrorText).toBe(expectedResult.emailErrorText);
   });
   // 실패
-  it('이메일이 형식에 맞지 않으면, 이메일 에러 텍스트를 반환한다.', () => {});
-  it('비밀번호가 형식에 맞지 않으면, 비밀번호 에러 텍스트를 반환한다.', () => {});
-  it('이메일과 비밀번호가 형식에 맞지 않으면, 이메일과 비밀번호 에러 텍스트를 반환한다.', () => {});
+  it('이메일이 형식에 맞지 않으면, 이메일 에러 텍스트를 반환한다.', () => {
+    // Given
+    const email = 'abcnaver.com';
+    const password = 'abc1234!@#$';
+    const expectedResult = {
+      emailErrorText: EMAIL_ERROR_TEXT,
+      passwordErrorText: '',
+      validateCredential: () => null,
+    };
+
+    // When
+    const { result } = renderHook(useValidateCredential);
+
+    act(() => {
+      result.current.validateCredential(email, password);
+    });
+
+    // Then
+    expect(result.current.emailErrorText).toBe(expectedResult.emailErrorText);
+  });
+
+  it('비밀번호가 형식에 맞지 않으면, 비밀번호 에러 텍스트를 반환한다.', () => {
+    // Given
+    const email = 'abc@naver.com';
+    const password = 'abc1234';
+    const expectedResult = {
+      emailErrorText: '',
+      passwordErrorText: PASSWORD_ERROR_TEXT,
+      validateCredential: () => null,
+    };
+
+    // When
+    const { result } = renderHook(useValidateCredential);
+
+    act(() => {
+      result.current.validateCredential(email, password);
+    });
+
+    // Then
+    expect(result.current.passwordErrorText).toBe(
+      expectedResult.passwordErrorText
+    );
+  });
+  it('이메일과 비밀번호가 형식에 맞지 않으면, 이메일과 비밀번호 에러 텍스트를 반환한다.', () => {
+    // Given
+    const email = 'abcnaver.com';
+    const password = 'abc1234';
+    const expectedResult = {
+      emailErrorText: EMAIL_ERROR_TEXT,
+      passwordErrorText: PASSWORD_ERROR_TEXT,
+      validateCredential: () => null,
+    };
+
+    // When
+    const { result } = renderHook(useValidateCredential);
+
+    act(() => {
+      result.current.validateCredential(email, password);
+    });
+
+    // Then
+    expect(result.current.emailErrorText).toBe(expectedResult.emailErrorText);
+    expect(result.current.passwordErrorText).toBe(
+      expectedResult.passwordErrorText
+    );
+  });
 });
