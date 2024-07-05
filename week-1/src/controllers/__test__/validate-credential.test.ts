dc13667ea4579420709ee4dd5e8f74688018d6bc
Joy Kim <30822283+feel5ny@users.noreply.github.com>
2024-07-05 23:21:11 +0900

    [단위테스트] 3. 실패 케이스 작성

--- week-1/src/controllers/__test__/validate-credential.test.ts
+++ week-1/src/controllers/__test__/validate-credential.test.ts
@@ -35,9 +35,88 @@ describe('validate credential', () => {
   });
 
   // 실패 case
-  it('이메일이 존재하지 않으면, 이메일이 유효하지 않다고 반환해야한다.', () => {});
-  it('이메일 포맷이 맞지 않으면, 이메일이 유효하지 않다고 반환해야한다.', () => {});
-  it('비밀번호가 존재하지 않으면, 비밀번호가 유효하지 않다고 반환해야한다.', () => {});
-  it('비밀번호가 포맷이 맞지 않으면, 비밀번호가 유효하지 않다고 반환해야한다.', () => {});
-  it('이메일과 비밀번호가 모두 포맷이 맞지 않으면, 모두 유효하지 않다고 반환해야한다.', () => {});
+  it('이메일이 존재하지 않으면, 이메일이 유효하지 않다고 반환해야한다.', () => {
+    // Given
+    const email = '';
+    const password = '1234abcd!@#';
+
+    // When
+    const result = validateCredential(email, password);
+
+    // Then
+    expect(result).toEqual({
+      isValidEmail: false,
+      isValidPassword: true,
+      isValidAll: false,
+    });
+  });
+  it('이메일 포맷이 맞지 않으면, 이메일이 유효하지 않다고 반환해야한다.', () => {
+    // Given
+    const email = 'spartaabc.com';
+    const password = '1234abcd!@#';
+
+    // When
+    const result = validateCredential(email, password);
+
+    // Then
+    expect(result).toEqual({
+      isValidEmail: false,
+      isValidPassword: true,
+      isValidAll: false,
+    });
+  });
+  it('비밀번호가 존재하지 않으면, 비밀번호가 유효하지 않다고 반환해야한다.', () => {
+    // Given
+    const email = 'sparta@abc.com';
+    const password = '';
+    const restResult = {
+      isValidEmail: true,
+    };
+
+    // When
+    const result = validateCredential(email, password);
+
+    // Then
+    expect(result).toEqual({
+      ...restResult,
+      isValidPassword: false,
+      isValidAll: false,
+    });
+  });
+  it('비밀번호가 포맷이 맞지 않으면, 비밀번호가 유효하지 않다고 반환해야한다.', () => {
+    // Given
+    const email = 'sparta@abc.com';
+    const password = '1234';
+    const restResult = {
+      isValidEmail: true,
+    };
+
+    // When
+    const result = validateCredential(email, password);
+
+    // Then
+    expect(result).toEqual({
+      ...restResult,
+      isValidPassword: false,
+      isValidAll: false,
+    });
+  });
+  it('이메일과 비밀번호가 모두 포맷이 맞지 않으면, 모두 유효하지 않다고 반환해야한다.', () => {
+    // Given
+    const email = 'sparta@abc.com';
+    const password = '1234';
+    const restResult = {
+      isValidEmail: true,
+    };
+
+    // When
+    const result = validateCredential(email, password);
+
+    // Then
+    expect(result).toEqual({
+      ...restResult,
+      isValidPassword: false,
+      isValidAll: false,
+    });
+  });
 });
