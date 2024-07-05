efa51744529c942ee4d2c9606ea3e20b2c180403
Joy Kim <30822283+feel5ny@users.noreply.github.com>
2024-07-05 23:36:48 +0900

    [단위-hook 테스트] 2. 성공 케이스 작성

--- week-1/src/hooks/__test__/use-validate-credential.test.ts
+++ week-1/src/hooks/__test__/use-validate-credential.test.ts
@@ -1,7 +1,25 @@
+import { renderHook } from '@testing-library/react';
+import { useValidateCredential } from '../use-validate-credential';
+
 // useValidateCredential -> 에러텍스트를 관리하는 hook
 describe('useValidateCredential', () => {
   // 성공
-  it('이메일과 비밀번호가 형식에 맞으면, 에러 텍스트를 반환하지 않는다.', () => {});
+  it('이메일과 비밀번호가 형식에 맞으면, 에러 텍스트를 반환하지 않는다.', () => {
+    // Given
+    const email = 'abc@naver.com';
+    const password = 'abc1234!@#$';
+    const expectedResult = {
+      emailErrorText: '',
+      passwordErrorText: '',
+      validateCredential: () => null,
+    };
+
+    // When
+    const { result } = renderHook(useValidateCredential);
+
+    // Then
+    expect(result.current.emailErrorText).toBe(expectedResult.emailErrorText);
+  });
   // 실패
   it('이메일이 형식에 맞지 않으면, 이메일 에러 텍스트를 반환한다.', () => {});
   it('비밀번호가 형식에 맞지 않으면, 비밀번호 에러 텍스트를 반환한다.', () => {});
---
+++ week-1/src/hooks/use-validate-credential.ts
@@ -0,0 +1,7 @@
+export const useValidateCredential = () => {
+  return {
+    emailErrorText: '',
+    passwordErrorText: '',
+    validateCredential: () => null,
+  };
+};
