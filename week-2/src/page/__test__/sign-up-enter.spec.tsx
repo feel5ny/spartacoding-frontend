06fc2ccb47b6387ef27224a55517d497f412367a
Joy Kim <feel5.nayoung@gmail.com>
2024-07-15 22:22:49 +0900

    2. Green 단계

--- week-2/src/page/__test__/sign-up-enter.spec.tsx
+++ week-2/src/page/__test__/sign-up-enter.spec.tsx
@@ -4,8 +4,10 @@
  * * 로그인한 유저는 회원가입 페이지에 진입할 수 없다.
  */
 
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from '../../App';
+import { LocationDisplay } from '../../utils/test-setup/location-display';
+import { Wrapper } from '../../utils/test-setup/wrapper';
 
 describe('유저는 회원가입 페이지에 진입할 수 있다', () => {
   it('로그인을 하지 않은 유저는, 회원가입 페이지에 진입할 수 있다.', () => {
@@ -14,10 +16,19 @@ describe('유저는 회원가입 페이지에 진입할 수 있다', () => {
     const path = '/sign-up';
 
     // When
-    render(<App />);
+    render(<App />, {
+      wrapper: ({ children }) => {
+        return (
+          <Wrapper initialEntry={path}>
+            {children}
+            <LocationDisplay />
+          </Wrapper>
+        );
+      },
+    });
 
     // Then
-    expect(window.location.pathname).toHaveTextContent(path);
+    expect(screen.getByTestId('location-display')).toHaveTextContent(path);
   });
 
   it('로그인한 유저는, 회원가입 페이지에 진입할 수 없다.', () => {});
