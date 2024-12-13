5317d84dc47f5d2d151224a6f4c81025828b6db9
Joy Kim <feel5.nayoung@gmail.com>
2024-12-13 22:58:53 +0900

    [2. 할일을 입력할 때 데드라인 날짜가 오늘 날짜 미만이면 입력할 수 없다.] 테스트 작성

--- zz.home-work/src/App.test.tsx
+++ zz.home-work/src/App.test.tsx
@@ -1,6 +1,7 @@
 import { fireEvent, render } from '@testing-library/react';
 import App from './App';
 import { format } from 'date-fns';
+import { vi } from 'vitest';
 
 const 할일입력 = ({ text, date }: { text: string; date: string }) => {
   const AppRenderResult = render(<App />);
@@ -27,6 +28,30 @@ describe('TodoList 테스트', () => {
     const { getByTestId } = 할일입력({
       text: maxLimitText,
       date: format(new Date(), 'yyyy-MM-dd'),
+    });
+
+    // -------- Then --------
+    // 버튼을 클릭할 수 없다.
+    const todoFormButton = getByTestId('todo-form-button');
+    // ? 버튼의 disabled여부로 버튼 활성화 여부를 확인한다.
+    expect(todoFormButton).toBeDisabled();
+  });
+
+  test('할일을 입력할 때 데드라인 날짜가 오늘 날짜 미만이면 입력할 수 없다.', () => {
+    // -------- Given --------
+    // ! 테스트환경을 오늘날짜를 고정합니다.
+    const date = new Date(2024, 12, 1);
+
+    vi.useFakeTimers();
+    vi.setSystemTime(date);
+
+    const mockDeadline = new Date(2024, 11, 1);
+
+    // -------- When --------
+    // 데드라인 날짜가 오늘 날짜 미만이면
+    const { getByTestId } = 할일입력({
+      text: '테스트 데이터',
+      date: format(mockDeadline, 'yyyy-MM-dd'),
     });
 
     // -------- Then --------
