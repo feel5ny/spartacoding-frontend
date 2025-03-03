7a4b684fee81a11ad4ce5a582c0ffb0af80c43b0
hoyeongrande <hooy21c@gmail.com>
2025-03-04 00:16:50 +0900

    home work week 1

--- zz.home-work/src/App.test.tsx
+++ zz.home-work/src/App.test.tsx
@@ -1,3 +1,18 @@
-describe('Todo List', () => {
-  test('-', () => {});
+import { describe, test } from "vitest"
+
+describe('Todo Input의 글자 수는 100자 미만이어야 한다', () => {
+  const isTodoValid = (text: string) => text.length < 100;
+
+  test('99자 일 때 테스트 해보기', () => {
+    expect(isTodoValid("1".repeat(99))).toEqual(true)
+  });
+
+  test('100자 일 때 테스트 해보기', () => {
+    expect(isTodoValid("1".repeat(100))).toEqual(false)
+  });
+
+  test('테스트 데이터 입력', () => {
+    expect(isTodoValid("TEST TODO LIST")).toEqual(true);
+  });
+
 });
