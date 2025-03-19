af6189992b12e22f8af290ffb5ec1a2e9999e040
0biglife <0biglife@gmail.com>
2025-03-19 21:03:27 +0900

    feat: test files for ci setting

--- week-3/vite.config.ts
+++ week-3/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { configDefaults } from 'vitest/config';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    include: [
+      '**/__tests__/*.test.tsx',
+      '**/__tests__/**/*.test.tsx',
+      '**/__tests__/**/*.spec.tsx',
+    ],
+    exclude: [...configDefaults.exclude, '**/node_modules/**', '**/dist/**'],
+    setupFiles: './src/setupTests.ts',
+  },
+});
